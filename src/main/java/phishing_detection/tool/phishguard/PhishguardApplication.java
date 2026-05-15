31e854614cb1f4010a4d940e5746a4dc07ec9de6
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2026-05-15 22:21:49 +0530

    initial commit

---
+++ src/main/java/phishing_detection/tool/phishguard/PhishguardApplication.java
@@ -0,0 +1,13 @@
+package phishing_detection.tool.phishguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhishguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phishing_detection/tool/phishguard/PhishguardApplicationTests.java
@@ -0,0 +1,13 @@
+package phishing_detection.tool.phishguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
